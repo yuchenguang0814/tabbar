f697a61285db26aa0d37b4b113b9d8a115952add
Yu <546448821@qq.com>
2020-09-05 15:11:03 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <tab-bar>
-      <tab-bar-item path="/home" activeColor="blue">
-        <img slot="item-icon" src="./assets/images/home.svg" alt="">
-        <img slot="item-icon-active" src="./assets/images/home-filling.svg" alt="">
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item path="/category" activeColor="green">
-        <img slot="item-icon" src="./assets/images/category-normal.svg" alt="">
-        <img slot="item-icon-active" src="./assets/images/category- Selected.svg" alt="">
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item path="/cart">
-        <img slot="item-icon" src="./assets/images/shop-normal.svg" alt="">
-        <img slot="item-icon-active" src="./assets/images/shop-Selected.svg" alt="">
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item path="/profile">
-        <img slot="item-icon" src="./assets/images/user.svg" alt="">
-        <img slot="item-icon-active" src="./assets/images/user-filling.svg" alt="">
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import TabBar from './components/tabbar/TabBar'
-import TabBarItem from './components/tabbar/TabBarItem'
+import MainTabBar from './components/tabbar/MainTabBar'
 export default {
   name:'App',
   components: {
-    TabBar,
-    TabBarItem
+    MainTabBar
   }
 }
 </script>
